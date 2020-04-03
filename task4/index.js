194fd1f40a55db54d645b3ec44cf368d5fc89d28
a1ryxgod <58265691+a1ryxgod@users.noreply.github.com>
2020-04-03 15:42:43 +0300

    Add files via upload

---
+++ task4/index.js
@@ -0,0 +1,5 @@
+const my = "My";
+const name = "name";
+const is = "is";
+const fullName = my + name + is + "Viktor";
+console.log(fullName)
---
+++ task6/index.js
@@ -0,0 +1,3 @@
+  const num = 20;
+  const resultt = num > 10 && num < 30;
+  console.log(resultt);
